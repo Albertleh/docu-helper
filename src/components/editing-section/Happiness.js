9f2af96d5d2dce38a378517077fc6d0b35ab6c6c
Albert Lehninger <39347355+Albertleh@users.noreply.github.com>
2022-11-22 11:59:58 +0000

    hppiness.js

--- src/components/editing-section/Happiness.js
+++ src/components/editing-section/Happiness.js
@@ -23,12 +23,16 @@ const Happiness = () => {
   const veryhappytext = [
       `${name} kam heute sehr zufrieden in die Tagesstätte.`,
       `${name} ging heute mit einem Lächeln in die Tagesstätte und wirkte sehr zufrieden`,
-      `${name} war heute sehr gut aufgelegt und grüßte freundlich beim Betreten der Tagesstätte`,
+      `${name} war heute scheinbar sehr gut aufgelegt und grüßte freundlich beim Betreten der Tagesstätte`,
   ];
-  const happytext = ["happy1", "happy2", "happy3"];
-  const neutraltext = ["neutral1", "neutral2", "neutral3"];
-  const sadtext = ["sad1", "sad2", "sad3"];
-  const verysadtext = ["verysad1", "verysad2", "verysad3"];
+  const happytext = [`${name} kam heute allgemein zufrieden in die Tagesstätte.`, 
+                     `${name} ging heute wohlauf in die Tagesstätte und wirkte allgemein ziemlich zufrieden`];
+  const neutraltext = [`${name} wirkte heute beim Betreten der Werkstätte von der Zuftriedenheit relativ neutral .`, 
+                       `${name} zeigte heute eine relativ ausgewogene Grundstimmung und wirkte allgemein ausgeglichen.`];
+  const sadtext = [`${name} kam heute betrübt in die Tagesstätte und wirkte leicht schlecht gelaunt.`, 
+                   `${name} betrat heute wenig zufrieden in die Tagesstätte und zeigte unglückliches Verhalten.`];
+  const verysadtext = [`${name} kam heute sichtbar miserabel gelaunt in die Tagesstätte und verbalisierte immer wieder schlechte Stimmung.`, 
+                       `${name} wirkte beim Betreten der Tagesstätte sehr schlecht gelaunt und gestikulierte sich abweisend.`];
 
   const { setMoodText } = useContext(moodTextContext);
 
