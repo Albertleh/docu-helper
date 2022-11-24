b04fd1c40e406e861035034d23c5e2e2c3c6f4e4
Albert Lehninger <39347355+Albertleh@users.noreply.github.com>
2022-11-24 13:55:43 +0000

    revert changes

--- src/components/editing-section/ButtonElement.js
+++ src/components/editing-section/ButtonElement.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ButtonElement = (props) => {
 
-  var disabledStyles = '';
+  const [disabledStyles, setDisabledStyles] = useState(' brightness-50');
+  let enabledstyles = ''
 
   const ClickHelper = () => {
     props.onSelect(props.text);
+    console.log(props.text.length)
+
+    setDisabledStyles(' brightness-50');
   }
 
-  if ( props.disable === true ) {
-    disabledStyles = ' brightness-50'
-  } 
-
-
+  if ( props.disable === false ) {
+    enabledstyles = ' brightness-100'
+  } else {
+    enabledstyles = '';
+  }
 
   return (
     <button class="button rounded-xl" onClick={ClickHelper} disabled={props.disable}>
-      <img class={"mood-image" + disabledStyles} src={props.icon} alt="Icon" disabled={props.disable} />
-      <h3 class={"mood-text" + disabledStyles} disabled={props.disable}>
+      <img class={"mood-image" + disabledStyles + enabledstyles} src={props.icon} alt="Icon" disabled={props.disable} />
+      <h3 class={"mood-text" + disabledStyles + enabledstyles} disabled={props.disable}>
         <h4 href="#">{props.text} </h4>
       </h3>
     </button>
