c9bab68fd4bd63242e1d2875601ab0de51bec3c9
Albert Lehninger <39347355+Albertleh@users.noreply.github.com>
2022-11-25 11:04:21 +0000

    rendering list

--- src/components/editing-section/Happiness.js
+++ src/components/editing-section/Happiness.js
@@ -10,55 +10,73 @@ import sad from "../../assets/sad.png";
 import verysad from "../../assets/very-sad.png";
 
 const Happiness = () => {
+  const ITEMS = [
+    ["Sehr Gut", "../../assets/very-happy.png"],
+    ["Gut", "../../assets/happy.png"],
+    ["Neutral", "../../assets/neutral.png"],
+    ["Schlecht", "../../assets/sad.png"],
+    ["Sehr Schlecht", "../../assets/very-sad.png"],
+  ];
 
   const { name } = useContext(nameContext);
 
   let disablebutton = true;
-  if ( name.length === 0 ) {
+  if (name.length === 0) {
     disablebutton = true;
-  } else if ( name.length > 0 ) {
+  } else if (name.length > 0) {
     disablebutton = false;
   }
 
   const veryhappytext = [
-      `${name} kam heute sehr zufrieden in die Tagesstätte. `,
-      `${name} ging heute mit einem Lächeln in die Tagesstätte und wirkte sehr zufrieden. `,
-      `${name} war heute scheinbar sehr gut aufgelegt und grüßte freundlich beim Betreten der Tagesstätte. `,
+    `${name} kam heute sehr zufrieden in die Tagesstätte. `,
+    `${name} ging heute mit einem Lächeln in die Tagesstätte und wirkte sehr zufrieden. `,
+    `${name} war heute scheinbar sehr gut aufgelegt und grüßte freundlich beim Betreten der Tagesstätte. `,
   ];
-  const happytext = [`${name} kam heute allgemein zufrieden in die Tagesstätte. `, 
-                     `${name} ging heute wohlauf in die Tagesstätte und wirkte allgemein ziemlich zufrieden. `];
-  const neutraltext = [`${name} wirkte heute beim Betreten der Werkstätte von der Zuftriedenheit gemischt. `, 
-                       `${name} zeigte heute eine ausgeglichene Stimmung und wirkte allgemein neutral in seiner Zufriedenheit. `];
-  const sadtext = [`${name} kam heute betrübt in die Tagesstätte und wirkte leicht schlecht gelaunt. `, 
-                   `${name} betrat heute wenig zufrieden in die Tagesstätte und zeigte unglückliches Verhalten. `];
-  const verysadtext = [`${name} kam heute sichtbar miserabel gelaunt in die Tagesstätte und verbalisierte immer wieder schlechte Stimmung. `, 
-                       `${name} wirkte beim Betreten der Tagesstätte sehr schlecht gelaunt und gestikulierte sich abweisend. `];
+  const happytext = [
+    `${name} kam heute allgemein zufrieden in die Tagesstätte. `,
+    `${name} ging heute wohlauf in die Tagesstätte und wirkte allgemein ziemlich zufrieden. `,
+  ];
+  const neutraltext = [
+    `${name} wirkte heute beim Betreten der Werkstätte von der Zuftriedenheit gemischt. `,
+    `${name} zeigte heute eine ausgeglichene Stimmung und wirkte allgemein neutral in seiner Zufriedenheit. `,
+  ];
+  const sadtext = [
+    `${name} kam heute betrübt in die Tagesstätte und wirkte leicht schlecht gelaunt. `,
+    `${name} betrat heute wenig zufrieden in die Tagesstätte und zeigte unglückliches Verhalten. `,
+  ];
+  const verysadtext = [
+    `${name} kam heute sichtbar miserabel gelaunt in die Tagesstätte und verbalisierte immer wieder schlechte Stimmung. `,
+    `${name} wirkte beim Betreten der Tagesstätte sehr schlecht gelaunt und gestikulierte sich abweisend. `,
+  ];
 
   const { setMoodText } = useContext(moodTextContext);
 
-  const ACTION = {
-    EXZELLENT: "Sehr Gut",
-    GUT: "Gut",
-    NEUTRAL: "Neutral",
-    SCHLECHT: "Schlecht",
-    MISERABEL: "Sehr Schlecht",
-  };
-
   const selectMoodHandler = (text) => {
-    if (text === ACTION.EXZELLENT) {
+    if (text === ITEMS[0][0]) {
       setMoodText(
         veryhappytext[Math.floor(Math.random() * veryhappytext.length)]
       );
-    } else if (text === ACTION.GUT) {
+    } else if (text === ITEMS[1][0]) {
       setMoodText(happytext[Math.floor(Math.random() * happytext.length)]);
-    } else if (text === ACTION.NEUTRAL) {
+    } else if (text === ITEMS[2][0]) {
       setMoodText(neutraltext[Math.floor(Math.random() * neutraltext.length)]);
-    } else if (text === ACTION.SCHLECHT) {
+    } else if (text === ITEMS[3][0]) {
       setMoodText(sadtext[Math.floor(Math.random() * sadtext.length)]);
     } else {
       setMoodText(verysadtext[Math.floor(Math.random() * verysadtext.length)]);
     }
   };
+
+  let content = ITEMS.map((item) => {
+    return (
+      <ButtonElement
+        text={item[0]}
+        icon={item[1]}
+        disable={disablebutton}
+        onSelect={selectMoodHandler}
+      />
+    );
+  });
 
   return (
     <React.Fragment>
@@ -66,36 +84,7 @@ const Happiness = () => {
         Zufriedenheit
       </div>
       <div class="m-3 grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-        <ButtonElement
-          icon={veryhappy}
-          text={ACTION.EXZELLENT}
-          disable={disablebutton}
-          onSelect={selectMoodHandler}
-        />
-        <ButtonElement
-          icon={happy}
-          text={ACTION.GUT}
-          disable={disablebutton}
-          onSelect={selectMoodHandler}
-        />
-        <ButtonElement
-          icon={neutral}
-          text={ACTION.NEUTRAL}
-          disable={disablebutton}
-          onSelect={selectMoodHandler}
-        />
-        <ButtonElement
-          icon={sad}
-          text={ACTION.SCHLECHT}
-          disable={disablebutton}
-          onSelect={selectMoodHandler}
-        />
-        <ButtonElement
-          icon={verysad}
-          text={ACTION.MISERABEL}
-          disable={disablebutton}
-          onSelect={selectMoodHandler}
-        />
+        {content}
       </div>
     </React.Fragment>
   );
