15576c87fb41aa9b0fe6149d5161a8b743d14233
Albert Lehninger <39347355+Albertleh@users.noreply.github.com>
2022-11-26 14:14:35 +0000

    removing button

--- src/components/editing-section/NameInput.js
+++ src/components/editing-section/NameInput.js
@@ -8,33 +8,29 @@ const NameInput = () => {
   const [nameValid, setNameValid] = useState(undefined);
 
   const NameConfirmHandler = () => {
-    if (inputref.current.value == '') {
+    if (inputref.current.value === '') {
       setNameValid(false);
-      validstyles = ' focus:ring-red-500 text-red-500'
     } else {
-      setName(inputref.current.value);
       setNameValid(true);
-      validstyles = ' focus:ring-green-500 text-green-500'
     }
+    setName(inputref.current.value.trim());
   };
 
   let validstyles = '';
+  let validtextstyles = '';
   if ( nameValid === true ) {
-    validstyles = ' focus:ring-green-500 text-green-500'
+    validstyles = ' border-2 border-green-500 text-green-500'
+    validtextstyles = ' text-green-500'
   } else if ( nameValid === false ) {
-    validstyles = ' focus:ring-red-500 text-red-500'
+    validstyles = ' border-2 border-gray-500 text-gray-500'
+    validtextstyles = ' text-gray-500'
   }
 
   return (
-    <div className="pt-5 sm:pt-4 sm:mb-3 sm:ml-5 text-2xl sm:text-lg text-white flex flex-col sm:flex-row sm:justify-center items-center sm:items-center  ">
-      <span className="mb-5 sm:mb-0">Name des Klienten:</span>
-      <input ref={inputref} type="text" className="py-1 px-2 mb-5 sm:mb-0 sm:ml-5 rounded-lg text-gray-900" />
-      <button
-        onClick={NameConfirmHandler}
-        className={"button2" + validstyles }
-      >
-        Name bestätigen
-      </button>
+    <div className="pt-5 sm:pt-6 sm:mb-5 sm:ml-5 text-2xl sm:text-lg text-white flex flex-col sm:flex-row sm:justify-center items-center sm:items-center  ">
+      <span className={"mb-5 sm:mb-0" + validtextstyles}>Name des Klienten:</span>
+      <input ref={inputref} type="text" onChange={NameConfirmHandler} 
+        className={"py-1 px-2 mb-5 sm:mb-0 sm:ml-5 rounded-lg text-gray-900" + validstyles}/>
     </div>
   );
 };
